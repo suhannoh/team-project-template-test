128b7ebf199f2bb9f9987d599e73739974495fcb
suhannoh <suhannoh@gmail.com>
2026-01-28 10:10:16 +0900

    branch 추가

---
+++ src/main/java/com/yonsai/books/service/BookService.java
@@ -0,0 +1,11 @@
+package com.yonsai.books.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    public void testMethod() {
+        return;
+    }
+}
