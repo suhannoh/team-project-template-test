6c8327e0f92edfa58e24368345a18371418069f4
suhannoh <suhannoh@gmail.com>
2026-01-29 11:46:13 +0900

    feature/update 구현

--- src/main/java/com/yonsai/books/controller/BookController.java
+++ src/main/java/com/yonsai/books/controller/BookController.java
@@ -1,9 +1,10 @@
 package com.yonsai.books.controller;
 
-import com.yonsai.books.dto.BookAddRequest;
+import com.yonsai.books.dto.BookUpdateRequest;
 import com.yonsai.books.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,29 +28,25 @@ public class BookController {
     private final BookService bookService;
 
     /**
-     * 도서 추가 요청 API
-     * 설명 :
-     *  - 도서 정보를 전달받아 기존에 있는지 확인하고 도서를 추가한다
-     * @param
-     *   request {
-     *      category - 카테고리
-     *      title - 제목
-     *      author - 작성자
-     *      description - 설명
-     *      price - 가격
-     *      discount - 할인율
-     *      pages - 페이지 수
-     *      stock - 재고 수량
-     *   }
-     * @return
-     *     200 : 저장 성공
-     *     400 : 사용자 도서 추가시 유효성 검사 실패
-     *     500 : 서버 오류
+     * 도서 수정을 담당하는 API
+     * - 도서 ID 와 수정할 데이터를 받아 Service에 전달한다
+     * - 이때 전달받는 데이터는 BookUpdateRequest와 매핑하여 전달한다
+     *
+     * 주의
+     *  - 수정이 필요로하는 부분만 전달하면 에러가 날 수 있음
+     *  - 수정이 불필요한 부분도 모두 작성 후 전달해야함
+     *
+     *  todo 수정이 필요한 부분만 받아 null 검사 후 수정할 수 있게 만들 수 있음 ,
+     *       지금은 특정 도서 조회 후 수정을 기준으로 하여 수정할 도서의 데이터를 모두 가지고 온다는 가정하에 진행 중
+     *       + 수정이 가능한 권한부여 및 검사가 필요함 , 지금은 도서만 생각하여 진행중이라 일단 넘어감
+     *
+     * @param bookId
+     * @param request
+     * @return 200 / 400 / 500
      */
-    @PostMapping("/book/add")
-    public ResponseEntity<Void> addBook ( @Valid @RequestBody BookAddRequest request) {
-        bookService.findOrCreateBook(request);
+    @PostMapping("/book/update/{bookId}")
+    public ResponseEntity<Void> updateBook (@PathVariable Long bookId,  @RequestBody @Valid BookUpdateRequest request) {
+        bookService.updateByBook(bookId, request);
         return ResponseEntity.ok().build();
     }
-
 }
--- src/test/java/com/yonsai/books/BooksApplicationTests.java
+++ src/test/java/com/yonsai/books/BooksApplicationTests.java
@@ -48,13 +48,14 @@ class BooksApplicationTests {
   void DTO_VALID_ERROR () throws Exception {
     String jsonRequest = """
         {
-          "category": "",
-          "title": "",
-          "author": "",
-          "description": "",
-          "price": -20000,
-          "discount": 101,
-          "pages": 5
+          "category": "소설",
+          "title": "스프링 부트 완벽 가이드 테스트 44",
+          "author": "노수한",
+          "description": "JPA와 스프링 부트를 활용한 책 관리 프로젝트 예제입니다.",
+          "price": 15000,
+          "discount": 20,
+          "pages": 320,
+          "stock": 10
         }
         """;
 
@@ -62,5 +63,26 @@ class BooksApplicationTests {
                     .contentType(MediaType.APPLICATION_JSON) // json 데이터를 전달할 때 사용
                     .content(jsonRequest)) // json 테스트 데이터
                     .andExpect(status().is4xxClientError()); // 상태값이 400 error면 ok
+  }
+
+
+  @Test
+  void 책_수정_컨트롤러_테스트 () throws Exception {
+    String jsonRequest = """
+         {
+          "category": "테스트",
+          "title": "테스트 제목 테스트 제목",
+          "author": "테스트 저자 테스트 저자",
+          "description": "테스트 내용 테스트 내용",
+          "price": 20000,
+          "discount": 40,
+          "pages": 3200,
+          "stock": 50
+        }
+        """;
+    mockMvc.perform(post("/book/update/2")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(jsonRequest))
+            .andExpect(status().isOk());
   }
 }
